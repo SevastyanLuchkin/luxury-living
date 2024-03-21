db4f2ed6db15003795a991300dde458165cd5256
sevastyan.luchkin <sevastyan.luchkin@x5.ru>
2024-03-21 22:10:37 +0300

    change order for products by brand

--- src/main/java/ru/luxury/living/controller/ProductController.java
+++ src/main/java/ru/luxury/living/controller/ProductController.java
@@ -59,7 +59,7 @@ public class ProductController {
             @RequestParam(required = false) List<Long> collectionIds,
             @RequestParam(required = false) Long typeId,
             @RequestParam(required = false, defaultValue = "true") Boolean inStock,
-            @ParameterObject @PageableDefault(sort = {"brand_number"}, direction = Sort.Direction.ASC) Pageable pageable
+            @ParameterObject @PageableDefault(sort = {"brand_number, brand_title"}, direction = Sort.Direction.ASC) Pageable pageable
     ) {
         return productService.search(brandIds, categoryId, collectionIds, typeId, inStock, pageable);
     }
