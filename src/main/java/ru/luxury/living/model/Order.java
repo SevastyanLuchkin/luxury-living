60dc4e8afcc7dd215f5461fecc1dddcaea059723
sevastyan.luchkin <sevastyan.luchkin@x5.ru>
2024-03-23 17:42:27 +0300

    max file size 5mb

--- src/main/java/ru/luxury/living/model/Order.java
+++ src/main/java/ru/luxury/living/model/Order.java
@@ -1,6 +1,7 @@
 package ru.luxury.living.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
 @Getter
 @Setter
 @Entity
+@Table(name = "orders")
 @Accessors(chain = true)
 public class Order extends BaseEntity {
 
