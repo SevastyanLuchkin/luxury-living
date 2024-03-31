77d4488f0f534253494d5a907431ce5485d496a4
sevastyan.luchkin <sevastyan.luchkin@x5.ru>
2024-03-31 13:14:19 +0300

    add partial upload

--- src/main/java/ru/luxury/living/controller/CatalogController.java
+++ src/main/java/ru/luxury/living/controller/CatalogController.java
@@ -1,6 +1,7 @@
 package ru.luxury.living.controller;
 
 import com.poiji.bind.Poiji;
+import com.poiji.option.PoijiOptions;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,9 +48,24 @@ public class CatalogController {
     private final ProductRepository productRepository;
     private final CollectionRepository collectionRepository;
 
+    @PutMapping
+    @Transactional
+    public void upload(@RequestParam("file") MultipartFile file, @RequestParam int line) throws IOException {
+        try (InputStream is = file.getInputStream()) {
+            XSSFWorkbook wb = new XSSFWorkbook(is);
+
+            List<CatalogExcel> catalog =
+                    Poiji.fromExcel(wb.getSheetAt(0), CatalogExcel.class, PoijiOptions.PoijiOptionsBuilder.settings(line).build())
+                            .stream().toList();
+
+            upload(catalog);
+        }
+    }
+
+
     @PostMapping
     @Transactional
-    public void parse(@RequestParam("file") MultipartFile file) {
+    public void parse(@RequestParam("file") MultipartFile file, @RequestParam Long line) {
         try (InputStream is = file.getInputStream()) {
             XSSFWorkbook wb = new XSSFWorkbook(is);
 
@@ -56,62 +73,67 @@ public class CatalogController {
                     Poiji.fromExcel(wb.getSheetAt(0), CatalogExcel.class)
                             .stream().toList();
 
-            Map<String, Brand> brandByName = catalog.stream()
-                    .map(row -> {
-                        Brand brand = brandRepository.findByTitleIgnoreCase(row.getBrand())
-                                .orElseGet(() -> new Brand().setTitle(row.getBrand()));
-
-                        Category category = categoryRepository.findByTitleIgnoreCase(row.getCategory())
-                                .orElseGet(() -> categoryRepository.save(new Category().setTitle(row.getCategory())));
-                        brand.getCategory().add(category);
-
-                        if (StringUtils.hasText(row.getType())) {
-                            Type type = typeRepository.findByTitleIgnoreCase(row.getType())
-                                    .orElseGet(() -> typeRepository.save(new Type().setTitle(row.getType())));
-                            category.getTypes().add(type);
-                        }
-                        return brandRepository.save(brand);
-                    })
-                    .distinct()
-                    .collect(Collectors.toMap(Brand::getTitle, Function.identity(), (x1, x2) -> x2));
-
-            Map<String, Category> categoryByName = catalog
-                    .stream()
-                    .map(CatalogExcel::getCategory)
-                    .distinct()
-                    .peek(System.out::println)
-                    .map(categoryName -> categoryRepository.findByTitleIgnoreCase(categoryName)
-                            .orElseThrow())
-                    .collect(Collectors.toMap(Category::getTitle, Function.identity(), (x1, x2) -> x2));
-
-            Map<String, Type> typeByName = catalog
-                    .stream()
-                    .filter(row -> StringUtils.hasText(row.getType()))
-                    .distinct()
-                    .map(row -> typeRepository.findByTitleIgnoreCase(row.getType())
-                            .orElseThrow())
-                    .collect(Collectors.toMap(Type::getTitle, Function.identity(), (x1, x2) -> x2));
-
-            catalog.stream()
-                    .map(row -> new Product()
-                            .setArticle(row.getArticle())
-                            .setTitle(row.getName())
-                            .setDescription(row.getDescription())
-                            .setType(StringUtils.hasText(row.getType()) ? typeRepository.findByTitleIgnoreCase(row.getType()).orElseThrow() : null)
-                            .setCategories(Set.of(categoryRepository.findByTitleIgnoreCase(row.getCategory()).orElseThrow()))
-                            .setBrand(brandRepository.findByTitleIgnoreCase(row.getBrand()).orElseThrow())
-                            .setMaterials(row.getMaterials())
-                            .setCountry(row.getCountry())
-                            .setVolume(row.getVolume())
-                            .setPrice(getPrice(row))
-                            .setInStock(Objects.equals("+", row.getInStock()))
-                    ).forEach(productRepository::save);
-
-            log.info("finished {}", catalog.size());
+            upload(catalog);
 
         } catch (Exception e) {
             log.error("Ошибка при попытке чтения excel файла", e);
         }
+    }
+
+    private void upload(List<CatalogExcel> catalog) {
+        Map<String, Brand> brandByName = catalog.stream()
+                .map(row -> {
+                    Brand brand = brandRepository.findByTitleIgnoreCase(row.getBrand())
+                            .orElseGet(() -> new Brand().setTitle(row.getBrand()));
+
+                    Category category = categoryRepository.findByTitleIgnoreCase(row.getCategory())
+                            .orElseGet(() -> categoryRepository.save(new Category().setTitle(row.getCategory())));
+                    brand.getCategory().add(category);
+
+                    if (StringUtils.hasText(row.getType())) {
+                        Type type = typeRepository.findByTitleIgnoreCase(row.getType())
+                                .orElseGet(() -> typeRepository.save(new Type().setTitle(row.getType())));
+                        category.getTypes().add(type);
+                    }
+                    return brandRepository.save(brand);
+                })
+                .distinct()
+                .collect(Collectors.toMap(Brand::getTitle, Function.identity(), (x1, x2) -> x2));
+
+        Map<String, Category> categoryByName = catalog
+                .stream()
+                .map(CatalogExcel::getCategory)
+                .distinct()
+                .peek(System.out::println)
+                .map(categoryName -> categoryRepository.findByTitleIgnoreCase(categoryName)
+                        .orElseThrow())
+                .collect(Collectors.toMap(Category::getTitle, Function.identity(), (x1, x2) -> x2));
+
+        Map<String, Type> typeByName = catalog
+                .stream()
+                .filter(row -> StringUtils.hasText(row.getType()))
+                .distinct()
+                .map(row -> typeRepository.findByTitleIgnoreCase(row.getType())
+                        .orElseThrow())
+                .collect(Collectors.toMap(Type::getTitle, Function.identity(), (x1, x2) -> x2));
+
+        catalog.stream()
+                .map(row -> new Product()
+                        .setArticle(row.getArticle())
+                        .setTitle(row.getName())
+                        .setDescription(row.getDescription())
+                        .setType(StringUtils.hasText(row.getType()) ? typeRepository.findByTitleIgnoreCase(row.getType()).orElseThrow() : null)
+                        .setCategories(Set.of(categoryRepository.findByTitleIgnoreCase(row.getCategory()).orElseThrow()))
+                        .setBrand(brandRepository.findByTitleIgnoreCase(row.getBrand()).orElseThrow())
+                        .setMaterials(row.getMaterials())
+                        .setCountry(row.getCountry())
+                        .setVolume(row.getVolume())
+                        .setPrice(getPrice(row))
+                        .setCollectionTitle(row.getCollection())
+                        .setInStock(Objects.equals("+", row.getInStock()))
+                ).forEach(productRepository::save);
+
+        log.info("finished {}", catalog.size());
     }
 
     @PostMapping("collections")
