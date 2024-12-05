8f9abbebfa7311f424c76f4248ecd862c38553b0
sevastyan.luchkin <sevastyan.luchkin@x5.ru>
2024-12-05 22:02:42 +0300

    fix for number

--- src/main/java/ru/luxury/living/repository/ProductRepository.java
+++ src/main/java/ru/luxury/living/repository/ProductRepository.java
@@ -1,13 +1,12 @@
 package ru.luxury.living.repository;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.luxury.living.model.Product;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
@@ -42,6 +41,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             select p from Product p where lower(p.title) = lower(:name) and lower(p.brand.title) = lower(:brand)
             """)
     List<Product> findByTitleIgnoreCaseAndByBrandTitle(String name, String brand);
+
+    Page<Product> findAllByActive(boolean active, Pageable pageable);
 
     List<Product> findByTitleIgnoreCaseAndAndDescriptionIgnoreCase(String title, String description);
 
